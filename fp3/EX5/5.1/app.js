ff44eed5cb266def8c7f7a7625cafe5968492dd7
MoreiraJorge <moreirajorge_17@outlook.pt>
2020-04-02 18:39:01 +0100

    done ex5.1

--- fp3/EX5/5.1/app.js
+++ fp3/EX5/5.1/app.js
@@ -17,7 +17,7 @@ function response(lat, lon, max){
         }
     }
 
-    xhttp.open("GET", `https://api.openchargemap.io/v3/poi/?output=json&latitude=${lat}&longitude==${lon}&maxresults==${max}`, true);
+    xhttp.open("GET", `https://api.openchargemap.io/v3/poi/?output=json&latitude=${lat}&longitude=${lon}&maxresults=${max}`, true);
     xhttp.setRequestHeader('Accept', 'application/json');
     xhttp.send();
 
@@ -25,4 +25,18 @@ function response(lat, lon, max){
 
 function process(response){
     console.log(response);
+    let tbody = document.getElementById("tbody");
+
+    for(let i = 0; i < response.length; i++){
+        let row = tbody.insertRow(-1);
+
+        row.insertCell(0).innerHTML = `${ i }`;
+        row.insertCell(1).innerHTML = response[i]["AddressInfo"]["Country"]["Title"];
+        row.insertCell(2).innerHTML = response[i]["AddressInfo"]["AddressLine1"];
+        row.insertCell(3).innerHTML = response[i]["AddressInfo"]["Town"];
+        row.insertCell(4).innerHTML = response[i]["AddressInfo"]["Postcode"];
+        
+    }
+
+    document.getElementById("paw-results-row").style.display = "block";
 }
