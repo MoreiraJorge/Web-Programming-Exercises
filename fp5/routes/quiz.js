a4a2cb74c2a1ca01801461ffe75bb64bd70399b2
MoreiraJorge <moreirajorge_17@outlook.pt>
2020-04-19 16:07:13 +0100

    1.4 undefined middleware bug

--- fp5/routes/quiz.js
+++ fp5/routes/quiz.js
@@ -17,47 +17,63 @@ router
 		res.render('pages/homepage')
 	})
 
-	/*
-	.use(function(req,res,next){
+	.use(function (req,_,next) {
+
+		const errors = {}
 
 		const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+		
 		if (re.test(req.body.email) == false) {
-			res.send("Email Invalido")
+			errors.email = "Email Invalido"
 		}
+		
+		console.log(req.body.email);
 
+		
 		if (typeof req.body.name !== 'string') {
-			res.send("Nome Invalido")
+			errors.name = "Nome Invalido"
 		}
 
+		/*
 		if (typeof req.body.Description !== 'string') {
-			res.send("Descrição Invalida")
+			errors.Description = "Descrição Invalida"
 		}
 
 		if (typeof req.body.Book !== 'string') {
-			res.send("Livro Invalido")
+			errors.Book = "Livro Invalido"
 		}
+
+		if (Object.keys(errors).length) {
+			req.errors = errors
+		}
+		*/
 
 		next();
 	})
-	*/
-	.post('/', upload.single('imageFile'),(req, res) => {
 
-		const dbPath = path.resolve('db', 'posts.json')
-		const dataRaw = fs.readFileSync(dbPath) || '[]'
-		const data = JSON.parse(dataRaw.toString())
-		data.push(req.body)
-		fs.writeFileSync(dbPath, JSON.stringify(data))
+	.post('/', upload.single('imageFile'), (req, res) => {
 
-		res.render('pages/review', {
-			name: req.body.name,
-			email: req.body.email,
-			book: req.body.Book,
-			description: req.body.Description,
-			read: req.body.read,
-			image: `/images/${ req.file.filename }`
-		})
+		if (req.errors) {
+			res.status(400)
+			res.send(req.errors)
+		} else {
 
+			const dbPath = path.resolve('db', 'posts.json')
+			const dataRaw = fs.readFileSync(dbPath) || '[]'
+			const data = JSON.parse(dataRaw.toString())
+			data.push(req.body)
+			fs.writeFileSync(dbPath, JSON.stringify(data))
+
+			res.render('pages/review', {
+				name: req.body.name,
+				email: req.body.email,
+				book: req.body.Book,
+				description: req.body.Description,
+				read: req.body.read,
+				image: `/images/${req.file.filename}`
+			})
+		}
 	})
 
 module.exports = router
