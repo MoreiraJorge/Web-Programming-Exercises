3540cd0d9133f34ab770f0f668aab69b3d0c8a92
MoreiraJorge <moreirajorge_17@outlook.pt>
2020-05-03 22:09:05 +0100

    add swagger with bug

--- fp7/RestAPI/api/index.js
+++ fp7/RestAPI/api/index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 
 const productsRouter = require('./products-router')
+const DocRouter = require('./api-doc-router')
+
 const apiRouter = express.Router()
 
+/**
+ * @swagger
+ * /:
+ *  get:
+ *      descritpion: used to get api status
+ *          responses:
+ *              '200':
+ *                  description: A successful response
+ */
 apiRouter.get('/', (req, res) => {
 	res.send({
 		status: 'ok'
@@ -10,5 +21,6 @@ apiRouter.get('/', (req, res) => {
 })
 
 apiRouter.use('/products', productsRouter)
+apiRouter.use('/doc', DocRouter)
 
 module.exports = apiRouter
